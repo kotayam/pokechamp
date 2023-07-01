a31021bb5bfa6996e458e5c45505e07504e9ea8a
ykomako4.9@gmail.com <ykomako4.9@gmail.com>
2023-07-01 12:01:57 -0400

    added loading message

--- frontend/main.js
+++ frontend/main.js
@@ -5,6 +5,8 @@ const DB_APILINK = 'https://pcbackend.heppoko.space/api/v1/pokechamp/';
 // const DB_APILINK = 'http://localhost:8000/api/v1/pokechamp/';
 
 window.onload = function() {
+  const loadingBox = document.querySelector(".loding-box");
+  loadingBox.style.display = "none";
   returnParties(DB_APILINK + `home`);
 }
 
